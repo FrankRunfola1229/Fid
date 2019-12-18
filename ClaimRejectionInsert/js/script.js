cde1ac50ba49efdadd64de6bb0406da4881fb04f
Runfola,Frank <runfola.frank@healthnow.org>
2019-12-18 09:30:10 -0500

    add files

---
+++ ClaimRejectionInsert/js/script.js
@@ -0,0 +1,51 @@
+﻿
+/**$(window).load(function () {
+    $("#loadContainer > img").fadeOut(500);
+    //document.querySelector("#loadingSpinner").style.display = "none"; //makes page more lightweight     
+});
+**/
+
+$(function() {
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//                     DISABLE SUBMIT INPUT TO DELETE WHEN NO CLAIMS
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	$(document).ready(function() {
+		document.getElementById("loadingGears").style.display = "none";
+
+		var listCount = $("#myTable tbody tr").length;
+
+		if (listCount < 1) {
+			$("input[name='removeSelected']").attr("disabled", true);
+			$("#select-all").attr("disabled", true);
+		}
+		else {
+			$("input[name='removeSelected']").attr("disabled", false);
+			$("#select-all").attr("disabled", false);
+		}
+	});
+
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	///                                    BUTTON
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	$("#select-all").click(function() {
+		var all = $("#select-all")[0];
+		all.checked = !all.checked; // toggle
+		var checked = all.checked;
+		$("input.select-item").each(function(index, item) {
+			item.checked = checked;
+		});
+	});
+
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	///                                      INPUT
+	/// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	$("input.select-item").click(function() {
+		var checked = this.checked;
+		console.log(checked);
+
+		var all = $("input.select-all")[0]; // checkSelected();
+		var total = $("input.select-item").length;
+		var len = $("input.select-item:checked:checked").length;
+		all.checked = len === total;
+	});
+});
